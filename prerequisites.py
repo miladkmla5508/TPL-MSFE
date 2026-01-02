8bc8a1c073aad444551f7643e8b326480609d34f
Milad Kamali <miladkmla1376@gmail.com>
2026-01-02 18:47:19 +0330

    Adding Libraries

---
+++ prerequisites.py
@@ -0,0 +1,55 @@
+!pip install yfinance==0.2.61
+!pip install scikit-learn==1.4.2
+!pip install pyti
+!pip install ta
+!pip install --upgrade mplfinance
+!pip install shap
+!pip install openpyxl
+!pip install ta-lib
+!pip install pandas_market_calendars
+!pip install statsmodels
+!pip install sklearn_relief
+!pip install mlxtend
+!pip install xgboost==2.0.3
+!pip install catboost
+!pip install lightgbm
+
+
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib
+import mplfinance
+import yfinance as yf
+import sklearn
+import ta
+import pyti
+import shap
+import openpyxl
+import talib 
+import pandas_market_calendars
+import sklearn_relief
+import statsmodels
+import mlxtend
+import xgboost
+import catboost 
+import lightgbm
+
+
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Seaborn:", sns.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("mplfinance:", mplfinance.__version__)
+print("yfinance:", yf.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("shap:", shap.__version__)
+print("openpyxl:", openpyxl.__version__)
+print("TA-Lib:", talib.__version__)
+print("pandas_market_calendars:", pandas_market_calendars.__version__)
+print("statsmodels:", statsmodels.__version__)
+print("mlxtend:", mlxtend.__version__)
+print("xgboost:", xgboost.__version__)
+print("catboost:", catboost.__version__)
+print("lightgbm:", lightgbm.__version__)
